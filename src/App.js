f66a1ed711f1722d5b793838ee8a8fd8f704a388
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-04-27 16:56:27 -0400

    main

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CartContainer from './pages/CartContainer'
 import Navbar from './components/Navbar'
 import { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 
 import { totalCart } from './features/cart/cartSlice'
 
 const App = () => {
-  const { cartItems } = useSelector((state) => state.cart)
+  // const { cartItems } = useSelector((state) => state.cart)
+  // console.log(cartItems)
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(totalCart())
-  }, [cartItems])
+  })
   return (
     <BrowserRouter>
       <Routes>
